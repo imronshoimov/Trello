5e0882fc15ffe18e9452631c1ea642d5b19dc4b7
imronshoimov <imronshoimov125@gmail.com>
2021-10-12 08:12:29 -0700

    changes

--- src/routes/api/cards.js
+++ src/routes/api/cards.js
@@ -17,6 +17,6 @@ router.post("/tasks", fileUpload().single("image"), validateTasks, insertData);
 router.put("/tasks/:id", fileUpload().single("image"), validateTasks, updateData);
 router.delete("/tasks/:id", deleteData);
 router.patch("/tasks/status/:id", updateStatus);
-router.get("/tasks/filter", filterData)
+router.get("/tasks/filter", filterData);
 
 module.exports = router;
