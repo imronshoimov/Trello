ba11fb8bfce857073b8934d8a51650d448075a1d
imronshoimov <imronshoimov125@gmail.com>
2021-10-13 10:40:03 -0700

    finish

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
-router.use("/api", (req, res) => res.status(404).json( { "No API route found" } ));
+router.use("/api", (req, res) => res.status(404).json( { message: "No API route found" } ));
 
 module.exports = router;
